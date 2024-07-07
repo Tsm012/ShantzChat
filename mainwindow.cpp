76f51b2193781aac7267055b14e6a2ec9f8d5ec3
Shantzformation <Tsm012@bucknell.edu>
2024-07-06 21:15:59 -0700

    load data into chat window

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QStringListModel>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -12,3 +13,13 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_pushButton_clicked()
+{
+    QStringList data;
+    data << "Item 1" << "Item 2" << "Item 3" << "Item 4" << "Item 5";
+    auto model = new QStringListModel(this);
+    model->setStringList(data);
+    ui->listView->setModel(model);
+}
+
